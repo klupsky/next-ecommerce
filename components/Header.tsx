ec091a2ac620a39dc6d42602361715190d61f644
klupsky <youremail@example.com>
2022-06-17 16:39:42 +0200

    try to make my cart different for drone to find total

--- components/Header.tsx
+++ components/Header.tsx
@@ -68,6 +68,7 @@ export default function Header(props: Props) {
             <a data-test-id="cart-count">{totalQuantity}</a>
           </div>
         </Link>{' '}
+        <a data-test-id="cart-count">{totalQuantity}</a>
       </div>
     </header>
   );
