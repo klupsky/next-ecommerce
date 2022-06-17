a3f3c5169ec480ac74ff87bcc29aed3c9d5dbe70
klupsky <youremail@example.com>
2022-06-17 16:28:53 +0200

    added pretty intendation to playwright testfile

--- migrations/00001-insert-products.js
+++ migrations/00001-insert-products.js
@@ -1,5 +1,5 @@
 const products = [
-  { id: '1', name: 'pink dot', type: 'happy', price: 1, color: '#ff92e9' },
+  { id: '1', name: 'pink dot', type: 'happy', price: 102, color: '#ff92e9' },
   { id: '2', name: 'green dot', type: 'stoned', price: 2, color: '#82c7a3' },
   { id: '3', name: 'red dot', type: 'rolling', price: 3, color: '#ff6843' },
   { id: '4', name: 'lilac dot', type: 'dreamy', price: 4, color: '#af99ff' },
